b303d283db9d48872a15c6acbaadef03b2a6e274
john9088 <35361130+john9088@users.noreply.github.com>
2018-03-18 22:55:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jason12/votingapp/Login.java
@@ -0,0 +1,77 @@
+package com.example.jason12.votingapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Login extends Activity {
+    TextView t3, tx;
+    Button b1, b2;
+    EditText e1,e2;
+    DataBase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        t3 = (TextView) findViewById(R.id.t3);
+        tx = (TextView) findViewById(R.id.tx);
+        b1 = (Button) findViewById(R.id.b1);
+        b2 = (Button) findViewById(R.id.b2);
+        e1 = (EditText)findViewById(R.id.e1);
+        e2 = (EditText)findViewById(R.id.e2);
+        db = new DataBase(this);
+
+        t3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainactivity = new Intent(Login.this, MainActivity.class);
+                startActivity(mainactivity);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent admin = new Intent(Login.this, admin.class);
+                startActivity(admin);
+
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String id = e1.getText().toString();
+                String pass = e2.getText().toString();
+                Cursor res = db.getdata();
+                if (res.getCount() == 0)
+                    Toast.makeText(Login.this, "EMPTY", Toast.LENGTH_SHORT).show();
+                else {
+
+                    while (res.moveToNext()) {
+                        if (id.equals(res.getString(1)) && pass.equals(res.getString(2))) {
+                            Bundle b = new Bundle();
+                            b.putString("PID",id);
+                            Intent vote = new Intent(Login.this,Vote.class);
+                            vote.putExtras(b);
+                            startActivity(vote);
+                            finish();
+                        } else
+                            Toast.makeText(Login.this, "ERROR...", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+
+            }
+        });
+
+    }
+}
+
---
+++ app/src/main/java/com/example/jason12/votingapp/Vote.java
@@ -0,0 +1,107 @@
+package com.example.jason12.votingapp;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class Vote extends AppCompatActivity {
+    Button b1,b2;
+    RadioGroup rg1;
+    RadioButton rb1,rb2,rb3,rb4,rb5;
+    DataBase db;
+    String id;
+    int vote;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vote);
+
+        b1 = (Button)findViewById(R.id.b1);
+        b2 = (Button)findViewById(R.id.b2);
+        rg1 = (RadioGroup)findViewById(R.id.rg1);
+        rb1 = (RadioButton)findViewById(R.id.rb1);
+        rb2 = (RadioButton)findViewById(R.id.rb2);
+        rb3 = (RadioButton)findViewById(R.id.rb3);
+        rb4 = (RadioButton)findViewById(R.id.rb4);
+        rb5 = (RadioButton)findViewById(R.id.rb5);
+        db = new DataBase(this);
+
+
+        Bundle b = getIntent().getExtras();
+        id = b.getString("PID");
+        //Toast.makeText(this, id, Toast.LENGTH_SHORT).show();
+
+        rg1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+
+                switch(checkedId){
+                    case R.id.rb1:
+                        vote = Integer.parseInt(b1.getText().toString());
+                        break;
+                    case R.id.rb2:
+                        vote = Integer.parseInt(rb2.getText().toString());
+                        break;
+                    case R.id.rb3:
+                        vote = Integer.parseInt(rb3.getText().toString());
+                        break;
+                    case R.id.rb4:
+                        vote = Integer.parseInt(rb4.getText().toString());
+                        break;
+                    case R.id.rb5:
+                        vote = Integer.parseInt(rb5.getText().toString());
+                        break;
+                }
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which){
+                            case DialogInterface.BUTTON_POSITIVE:
+                                boolean input = db.enterVote(id,vote);
+                                if(input == true){
+                                    Toast.makeText(Vote.this, "Thank-You", Toast.LENGTH_SHORT).show();
+                                }
+                                else
+                                    Toast.makeText(Vote.this, "Try Again", Toast.LENGTH_SHORT).show();
+
+                                Intent exit = new Intent(Vote.this,Exit.class);
+                                startActivity(exit);
+                                finish();
+                                break;
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                Toast.makeText(Vote.this, "ENTER VALID VOTE", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                    }
+                };
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(Vote.this);
+                builder.setMessage("Are you sure about your vote?").setPositiveButton("Yes", dialogClickListener)
+                        .setNegativeButton("No", dialogClickListener).show();
+            }
+        });
+
+
+    }
+}
